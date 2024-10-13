786effacd42bd153f0521e17e7fa0ef3cb709ac1
Luis Fernández <hello.lunof@gmail.com>
2024-10-13 10:53:16 -0600

    build: configure vitest as main test runner

---
+++ src/app/components/counter/counter.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+export function Counter() {
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => {
+    setCounter((previous) => previous + 1)
+  }
+
+  const label = `Count: ${counter}`
+
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="text-center text-sm text-slate-500">{label}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import { Counter } from '@/app/components/counter'
+
 export default function Home() {
   return (
     <main>
-      <div className="container mx-auto flex min-h-screen flex-col items-center justify-center gap-y-2 px-4">
-        <h1 className="text-center text-4xl font-thin text-slate-700">
-          Welcome to Tohru
-        </h1>
-        <p className="text-center text-sm text-slate-500">
-          Made with &#x1F49B; and JavaScript
-        </p>
+      <div className="container mx-auto flex min-h-screen flex-col items-center justify-center px-4">
+        <div className="gap-y-2">
+          <h1 className="text-center text-4xl font-thin text-slate-700">
+            Welcome to Tohru
+          </h1>
+          <p className="text-center text-sm text-slate-500">
+            Made with &#x1F49B; and JavaScript
+          </p>
+        </div>
+        <Counter />
       </div>
     </main>
   )
