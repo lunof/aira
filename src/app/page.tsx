d3a4ce80c9c8f4f7ddb15d2c1ade16f13abede19
Dayari Rodriguez <dayaricruz12@gmail.com>
2024-10-16 20:17:44 -0600

    feat: create my first component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Counter } from '@/app/components/counter'
+import { MyFirstComponent } from '@/app/components/my-first-component/my-first-component'
 import { Heading } from '@/components/ui/heading'
 import { Small } from '@/components/ui/small'
 
@@ -6,13 +6,14 @@ export default function Home() {
   return (
     <main>
       <div className="container mx-auto flex min-h-screen flex-col items-center justify-center gap-y-10 px-4">
-        <div className="flex flex-col items-center gap-y-2">
+        <div className="flex flex-col items-center gap-y-3">
           <Heading className="text-center">Welcome to Aira</Heading>
           <Small className="text-center">
             Made with &#x1F49B; and JavaScript
           </Small>
         </div>
-        <Counter />
+        <MyFirstComponent initialValue={10} />
+        <MyFirstComponent initialValue={20} incrementBy={10} />
       </div>
     </main>
   )
