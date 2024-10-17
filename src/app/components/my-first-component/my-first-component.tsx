0d97f3aa3f55dcf401d2f3a5bea5f5d99084199e
Saí Bello <sayo00080@gmail.com>
2024-10-16 19:59:37 -0600

    feat: create my first component

---
+++ src/app/components/my-first-component/my-first-component.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { ChangeEvent, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Heading } from '@/components/ui/heading'
+
+type MyFirstComponentProps = {
+  initialValue?: number
+  incrementBy?: number
+}
+
+export function MyFirstComponent({
+  initialValue = 0,
+  incrementBy = 1,
+}: MyFirstComponentProps) {
+  const [counter, setCount] = useState(initialValue)
+  const [fullName, setFullName] = useState('')
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFullName(event.target.value)
+  }
+  const increment = () => {
+    setCount((old) => old + incrementBy)
+  }
+  return (
+    <main className="flex flex-col items-center gap-y-3">
+      <Heading variant="h2">{fullName}</Heading>
+      <Heading variant="h3">Counter: {counter}</Heading>
+      <input
+        value={fullName}
+        onChange={onChange}
+        type="text"
+        className="bg-slate-100"
+      />
+      <Button onClick={increment}>Incrementar</Button>
+    </main>
+  )
+}
